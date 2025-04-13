e49f11bade28a94a12cd3cbf0f2a6580f74b43f9
Gabriel D. Couto <gabrieldocoutos@gmail.com>
2025-04-13 00:07:20 -0300

    wip

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -25,7 +25,7 @@ app.get("/hellow-world", async (request, reply) => {
 });
 
 const port = parseInt(process.env.PORT || "3001", 10);
-app.listen({ port }, (err, address) => {
+app.listen({ port, host: "0.0.0.0" }, (err, address) => {
 	if (err) {
 		app.log.error(err);
 		process.exit(1);
